295dc3183518e9555ea6437b4f12a2d9b854afd3
TBespalko19 <tbespalko19@gmail.com>
2024-01-30 02:04:14 +0100

    home work 2

---
+++ src/Main.java
@@ -0,0 +1,92 @@
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+// Task 1, 2
+//public class Main {
+    /*public static void main(String[] args) { // строки 5 - 48
+        byte a = 10;
+        short b = 12;
+        int c = 154;
+        long d = 1567;
+
+        float e = 1.5F;
+        double f = 2.5;
+
+        char g = '\u0000';
+
+        boolean h = true;
+
+        //System.out.println(a);
+        //System.out.println(b);
+        //System.out.println(c);
+        //System.out.println(d);
+        //System.out.println(e);
+        //System.out.println(f);
+        //System.out.println(g);
+        //System.out.println(h);
+
+
+        //task 2
+    //    System.out.println(a % b == 0);
+
+    //    System.out.println(c++); // c = c + 1
+    //    System.out.println(c);
+
+    //    System.out.println(++d);
+
+    //    System.out.println(--e);
+
+    //    System.out.println(f == b);
+
+    //    System.out.println(g >= f);
+    //    System.out.println(e != f);
+
+    //    System.out.println(a > b && c < d);
+    //    System.out.println(a > b || c < d);
+
+    //    System.out.println(!(5 < 3) || 4 > 10);
+
+    //    System.out.println(c << a); */
+
+
+        //Task 4
+    /*public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+
+    System.out.println("Введите число, для проверки на четность");
+    int input = in.nextInt();
+
+    if (input % 2 == 0) {
+        System.out.println("Число четное");
+    }
+
+    if (!(input % 2 == 0)) {
+        System.out.println("Число не четное");
+    }*/
+
+
+    //Task 3
+public class Main {
+        public static void main(String[] args) {
+            byte a = 23;
+            short b = 12;
+            System.out.println(a % b == 0);
+        }
+
+        public static int getNumber() {
+            int a = 5;
+            int b = 3;
+            return a + b;
+        }
+
+}
+        //SCANNER EXAMPLE
+        //Scanner scanner = new Scanner(System.in); // утилита, чтобы что-то вводить в консоль
+        //System.out.println(scanner.nextInt() > 10);
+        //int i = scanner.nextInt();
+        //int j = scanner.nextInt();
+        //int k = scanner.nextInt();
+
+        //if (i > j && i > k) {
+        //    System.out.println("i>j");
